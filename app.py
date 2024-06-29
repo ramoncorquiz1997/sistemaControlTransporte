91d3c0ae7e6ce9942d97b05ef968181daadcc93e
rcontreras <rcontreras@aliquammed.com>
2024-06-29 00:11:44 -0700

    Subo cambios a los archivos para la pagina de registro

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from routes import register_routes
 
 app = Flask(__name__)
 app.secret_key = 'ramon'  # Añade una clave secreta para sesiones
 
+# Configuración de la base de datos
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 # Configura las rutas
 register_routes(app)
+
+# Definir el modelo de usuario
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,20 +1,26 @@
 from flask import render_template, redirect, url_for, request, Blueprint
+from app import db, User
 
 auth_routes = Blueprint('auth_routes', __name__)
+
+@auth_routes.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        email = request.form['email']
+
+        new_user = User(firstname=firstname, lastname=lastname, email=email)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('auth_routes.login'))
+    
+    return render_template('register.html')
 
 @auth_routes.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        # Obtener los datos del formulario
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        email = request.form['email']
-        
-        # Aquí puedes añadir la lógica para validar y procesar los datos del formulario
-        print(f"Nombre: {firstname}, Apellido: {lastname}, Correo: {email}")
-
-        # Después de procesar los datos, redirige a una página de inicio o dashboard
         return redirect(url_for('main_routes.index'))
-    
-    # Si es GET, simplemente muestra el formulario de inicio de sesión
+
     return render_template('login.html')
