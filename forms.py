9e6799a8c784175c639433410d9967f37b5f9ee6
rcontreras <rcontreras@aliquammed.com>
2024-07-12 23:46:53 -0700

    Cambios en colores y estilos

--- forms.py
+++ forms.py
@@ -4,24 +4,24 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from models import User
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    username = StringField('Usuario', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirmar contraseña', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Registrarse')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('That username is taken. Please choose a different one.')
+            raise ValidationError('Este usuario no esta dispnible, escoge otro')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            raise ValidationError('That email is taken. Please choose a different one.')
+            raise ValidationError('Este usuario ya tiene un registro, favor de usar otro')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    username = StringField('Usuario', validators=[DataRequired(), Length(min=2, max=20)])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
+    remember = BooleanField('Recuerdame')
+    submit = SubmitField('Inicar sesion')
--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,43 +1,41 @@
-from flask import Blueprint, render_template, url_for, flash, redirect, request
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_user, current_user, logout_user, login_required
 from app import db, bcrypt
 from models import User
 from forms import RegistrationForm, LoginForm
-from flask_login import login_user, current_user, logout_user, login_required
 
 auth_routes = Blueprint('auth_routes', __name__)
+
+@auth_routes.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('main_routes.home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=True)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('main_routes.home'))
+        else:
+            flash('Inicio de sesión fallido. Por favor, verifica el usuario y la contraseña', 'danger')
+    return render_template('login.html', form=form)
+
+@auth_routes.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main_routes.home'))
 
 @auth_routes.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('main_routes.welcome'))
+        return redirect(url_for('main_routes.home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
-        flash('Tu cuenta ha sido creada con éxito!', 'success')
+        flash('Tu cuenta ha sido creada. ¡Ahora puedes iniciar sesión!', 'success')
         return redirect(url_for('auth_routes.login'))
-    return render_template('register.html', title='Register', form=form)
-
-@auth_routes.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('main_routes.welcome'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            flash('Has iniciado sesión con éxito!', 'success')
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('main_routes.welcome'))
-        else:
-            flash('Inicio de sesión fallido. Verifica tu correo y contraseña.', 'danger')
-    return render_template('login.html', title='Login', form=form)
-
-@auth_routes.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('main_routes.home'))
+    return render_template('register.html', form=form)
